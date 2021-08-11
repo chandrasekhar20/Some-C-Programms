b94e5ff079a75e80c463541cd30b3587ae4acf70
chandrasekhar20 <69673014+chandrasekhar20@users.noreply.github.com>
2021-08-11 15:39:38 +0530

    Add files via upload

---
+++ ArraysAsParameters/ArraysAsParameters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArraysAsParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = MinValue(10, 20, 30, 2, 3, 4, 5, -293434, 3, 56, 3, 53, 54);
+            Console.WriteLine("The smaller value in the given list is :" + minNumber);
+        }
+        public static int MinValue(params int[] numbers)
+        {
+            int minValue = int.MaxValue;
+            foreach(int number in numbers)
+            {
+                if (number < minValue)
+                    minValue = number;
+            }
+            return minValue;
+        }
+    }
+}
+
